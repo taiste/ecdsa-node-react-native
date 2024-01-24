04bd3cd250454a3eb93165d356a024b75919bfd6
Mikko Harju <mikko@taiste.fi>
2024-01-24 09:46:50 +0200

    Fixed index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,17 +1,27 @@
-declare module 'starkbank-ecdsa' {
+declare module "starkbank-ecdsa" {
   export module Ecdsa {
-    export async function sign(message: string, privateKey: PrivateKey, hashfunc: (s: string) => string = null, randNum: string = null): Signature;
-    export async function verify(message: string, signature: Signature, publicKey: PublicKey, hashfunc: (s: string) => string = null): bool;
+    export function sign(
+      message: string | ArrayBuffer,
+      privateKey: PrivateKey,
+      hashfunc?: (s: string | ArrayBuffer) => Promise<string>,
+      randNum?: string,
+    ): Promise<Signature>;
+    export function verify(
+      message: string | ArrayBuffer,
+      signature: Signature,
+      publicKey: PublicKey,
+      hashfunc?: (s: string | ArrayBuffer) => Promise<string>,
+    ): Promise<boolean>;
   }
   export class PublicKey {
-    static fromPem(publicKeyPem: string);
+    static fromPem(publicKeyPem: string): PublicKey;
   }
   export class PrivateKey {
-    static fromPem(publicKeyPem: string);
+    static fromPem(publicKeyPem: string): Signature;
   }
   export class Signature {
-    toDer();
-    toBase64();
-    static fromBase64(s: string);
+    toDer(): string;
+    toBase64(): string;
+    static fromBase64(s: string): Signature;
   }
 }
